c013830d7548af3fec11c342b5a03e41fe03d819
LV-23 <lokeshvyavhare23@gmail.com>
2022-11-12 02:21:06 +0530

    Server Response Optimization

--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -4,44 +4,55 @@ const User = require("./user.model");
 const app = express.Router();
 
 app.post("/signup", async (req, res) => {
-  try {
-    let reqUser = req.body;
-    reqUser.type='customer';
-    let user = await User.create(reqUser);
-    res.send({
-      error: false,
-      message: "Your account has been created",
-      token: `${user._id}-${user.email}-${user.type}-${user.password}`,
-    });
-  } catch (e) {
-    res.send({
-      error: true,
-      message: "Something went wrong. Please try again.",
-    });
+  let reqUser = req.body;
+  reqUser.type = 'customer';
+  let { email } = req.body;
+  let Curuser = await User.findOne({ email });
+  if (Curuser) {
+      res.send({
+        error:true,
+        message: "Email is already linked to another account."
+      })
+  } else {
+    try {
+      let user = await User.create(reqUser);
+      res.send({
+        error: false,
+        message: "Your account has been created",
+        token: `${user._id}-${user.email}-${user.type}-${user.password}`,
+      });
+    } catch (e) {
+      console.log(e)
+      res.send({
+        error: true,
+        message: e,
+      });
+    }
   }
 });
 
 app.post("/login", async (req, res) => {
   let { email, password } = req.body;
-  try {
-    let user = await User.findOne({ email });
-    console.log(password);
-    console.log(user);
-    if (user.password !== password) {
+  let user = await User.findOne({ email });
+
+  if (user) {
+    try {
+      if (user.password !== password) {
+        res.send({ error: true, message: "Wrong Passwod! Please try again." });
+      } else {
+        res.status(200).send({
+          error: false,
+          message: "You've logged in successfully.",
+          token: `${user._id}-${user.email}-${user.type}-${user.password}`,
+        });
+      }
+    } catch (e) {
       res
-        .status(404)
-        .send({ error: true, message: "Wrong Passwod! Please try again." });
-    } else {
-      res.status(200).send({
-        error: false,
-        message: "You've logged in successfully.",
-        token: `${user._id}-${user.email}-${user.type}-${user.password}`,
-      });
+        .status(401)
+        .send({ error: true, message: "Somthing went wrong. Please try again." });
     }
-  } catch (e) {
-    res
-      .status(401)
-      .send({ error: true, message: "Somthing went wrong. Please try again." });
+  } else {
+    res.send({ error: true, message: "No such user present/Invalid email" })
   }
 });
 
